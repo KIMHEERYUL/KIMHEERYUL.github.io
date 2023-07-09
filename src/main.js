a15187f997bf914327b10bfdbe2c83bab9d218e8
RYUL <112082668+KIMHEERYUL@users.noreply.github.com>
2023-07-09 18:17:33 +0900

    JS-arrowUP

--- src/main.js
+++ src/main.js
@@ -14,23 +14,19 @@ document.addEventListener('scroll',() => {
     }
 });
 
-
-// const home = document.querySelector('#home');
-// const homeHeight = home.getBoundingClientRect().height;
-// function chgHomeOpacity(){
-//     if(window.scrollY >homeHeight){
-//         home.style.opacity(0);
-//     }else{
-//         home.style.opacity(1);
-//     }
-// }
-// document.addEventListener('scroll', () => {
-    
-// });
-
 //Home섹션을 아래로 스크롤시 투명하게 처리
  const home = document.querySelector('.home__container');
  const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Arrow up버튼을 아래로 스크롤시 나타나게 함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2){
+        arrowUp.style.opacity = 1;
+    }else{
+        arrowUp.style.opacity = 0;
+    }
 });
